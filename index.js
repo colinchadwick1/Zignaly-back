8ec677fe99e173a221f5cb3de1493bd635b6bd38
Colin Chadwick <colinchadwick@hotmail.co.uk>
2022-02-24 11:23:17 +0100

    finished

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 // Import packages
-const express = require('express')
-const morgan = require('morgan')
-const data = require('./data.json')
+const express = require("express");
+const morgan = require("morgan");
+const data = require("./data.json");
 
 // App
-const app = express()
+const app = express();
 // Morgan
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 // First route
-app.get('/phones', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.json(data)
-})
+app.get("/phones", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.json(data);
+});
 
 app.get("/phones/:phoneId", (req, res, next) => {
-    const { phoneId } = req.params;
-    res.json(phoneId)
-  
-
-  });
-
+  const { phoneId } = req.params;
+  res.json(phoneId);
+});
 
 // Starting server
-app.listen('5005')
+app.listen("5005");
